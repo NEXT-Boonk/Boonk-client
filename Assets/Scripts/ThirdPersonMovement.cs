4a3a92fd20d0a342b3240b3a379d3028f10cf6eb
Maelkiz <karl.ruby2005@gmail.com>
2023-02-01 13:00:33 +0100

    Good
    Movement is fine
    Camera behaviour needs work but is currently alright

--- Assets/Scripts/ThirdPersonMovement.cs
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -4,51 +4,68 @@ using UnityEngine;
 
 public class ThirdPersonMovement : MonoBehaviour
 {
-    private CharacterController controller;
+    [SerializeField] private CharacterController controller;
     [SerializeField] private Transform cam;
-    [SerializeField] private float speed = 10.0f; 
-    [SerializeField] private float turnSmoothTime = 0.1f; 
-    [SerializeField] private float jumpHeight = 5.0f;
+    [SerializeField] private float moveSpeed; 
+    [SerializeField] private float jumpForce;
+    [SerializeField] private float gravity;
 
+    private float turnSmoothTime = 0.1f; 
     private float turnSmoothVelocity;
-    private float g = 9.81f;
-
+    private Vector3 velocity;
 
     void Awake()
     {
-        controller = GetComponent<CharacterController>();
-
+        // hides the cursor 
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
 
     void Update()
-    { 
+    {
+        ApplyGravity();  
+        Jump();  
+        Move();
+        controller.Move(velocity * Time.deltaTime); 
+    }
+    
+    private void ApplyGravity() 
+    {
+        if (!controller.isGrounded) velocity.y -= gravity * Time.deltaTime;
+        else velocity.y = -0.1f;
+    }
+
+    private void Move() 
+    {
+        // gets input
         float inputWS = Input.GetAxisRaw("Vertical");
         float inputAD = Input.GetAxisRaw("Horizontal");
+
+        // normalized direction vector
         Vector3 direction = new Vector3(inputAD, 0, inputWS).normalized;
-
-        if (direction.magnitude >= 0.1) {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-            transform.rotation = Quaternion.Euler(0f, angle, 0f);
-            
-            Vector3 moveDir = Quaternion.Euler(0.0f, targetAngle, 0.0f) * Vector3.forward;
-
-            // Applying gravity
-            if (!controller.isGrounded) moveDir.y += g * Time.deltaTime;
-            else if (direction.y < 0) direction.y = 0f;
-
-            controller.Move(moveDir * speed * Time.deltaTime); 
-        }
         
-        /*
-        // Jumping      
-        if (Input.GetButtonDown("Jump") && controller.isGrounded)
-        {
-            print("shit");
-            direction.y += Mathf.Sqrt(jumpHeight * -2.0f * g);
+        // no movement if the direction vector's magnitude is too close to zero
+        if (direction.magnitude >= 0.1) {
+            // a trig function is used to calculate the player's look direction
+            float lookDirectionAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            
+            // a smoothing function is used to smooth the rotation of the player model
+            float playerModelAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, lookDirectionAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, playerModelAngle, 0f);
+            
+            // the movement direction is calculated using the look direction
+            Vector3 moveDirection = Quaternion.Euler(0.0f, lookDirectionAngle, 0.0f) * Vector3.forward;
+
+            controller.Move(moveDirection.normalized * moveSpeed * Time.deltaTime); 
         }  
-        */    
+    }
+
+    private void Jump() 
+    {   
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (!controller.isGrounded) return;
+            velocity.y += jumpForce;
+        }   
     }
 }

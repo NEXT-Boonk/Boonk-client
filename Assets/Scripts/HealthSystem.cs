88a245bff831d83aaec08bba47ce6e76318f9adc
Moonatone <nickhuang0610@gmail.com>
2023-02-20 10:38:29 +0100

    merged

---
+++ Assets/Scripts/HealthSystem.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthSystem : MonoBehaviour
+{
+    public float maxHealth;
+    public float currentHealth;
+    public float damage;
+    public Slider healthBar;
+    public GameObject deathScreen;  
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        deathScreen.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthBar.value = currentHealth;
+        if(currentHealth > maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+
+        if (Input.anyKey)
+        {
+            DamageSystem();
+        }
+
+        if(currentHealth <= 0)
+        {
+            deathScreen.SetActive(true);
+        }
+
+    }
+
+    void DamageSystem()
+    {
+        TakeDamage();
+    }
+
+    void TakeDamage()
+    {
+        currentHealth -= damage;
+    }
+}
